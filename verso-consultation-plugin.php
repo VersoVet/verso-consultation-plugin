de743b3e8e451e3fad0e9351ffeabdde2a6bfd5e
OnyxLab Dev <hpc.verso@gmail.com>
2026-05-10 16:48:56 +0000

    chore: Bump version to 3.4.1

--- verso-consultation-plugin.php
+++ verso-consultation-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Verso Consultation Form
  * Plugin URI: https://github.com/VersoVet/verso-consultation-plugin
  * Description: Professional consultation form with email notifications and secure file uploads
- * Version: 3.4.0
+ * Version: 3.4.1
  * Author: Verso Vet
  * License: GPL v2 or later
  * Text Domain: verso-consultation-plugin
@@ -782,7 +782,7 @@ function verso_enqueue_scripts() {
         'verso-form-handler',
         plugin_dir_url(__FILE__) . 'js/form.js',
         ['jquery'],
-        '3.4.0',
+        '3.4.1',
         true
     );
 
